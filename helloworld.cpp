81cfc530c800954f8b5580beda79c3b3eabb81bb
polmr <pm@mega.nz>
2016-09-26 18:36:28 +0200

    salutes

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
 using namespace std;
+
+void salute01()
+{
+  cout <<"salute 01!" << endl;
+}
+
+void yujuu02()
+{
+  cout <<"salute 02!" << endl;  
+}
 int main(int argc, char** argv)
 {  
-  cout<<"Hello World from develop!";
-  cout<<"Said again!";
+  cout<<"Hello World from develop! br06" << endl;
+  cout<<"Said again!" << endl;
+  
+  salute01();
+  yujuu02();
+  
   return 0;
 }
