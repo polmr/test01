329aa96d3722fe92a6d7ebed829fb4008f4bddcd
Pablo Martin <pm@mega.nz>
2018-06-28 12:32:18 +0200

    10

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void salute01()
 {
-  cout <<"salute 01 tocado por develop, again 09!" << endl;
+  cout <<"salute 01 tocado por develop, again 010!" << endl;
 }
 
 void yujuu02()
